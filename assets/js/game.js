ea8799d640a14036ff802ec6cbc8cc108465c005
Vox Humana <jonprine@gmail.com>
2020-06-16 12:25:42 -0500

    added multiple enemies. moved some window alerts up in the code

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,21 +3,25 @@
 //      * Fight all enemy robots
 //      * Defeat each enemy robot
 // "LOSE" - Player robot's health is zero or less
+
+window.alert("Welcome to Robot Gladiators!");
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
+
 // You can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
 var playerMoney = 10;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     //Alert users that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -70,6 +74,8 @@ var fight = function() {
           }  else {
             window.alert("You need to pick a valid option. Try again!");
         }
-};
+}
 
-//fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
